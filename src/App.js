4229a76cb2c907ef02e3b0b778c527dda9d21f4e
Pete VS <pete_vs88@me.com>
2020-12-02 11:53:47 -0500

    added first components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
 import React from 'react';
+import TodoList from './components/TodoList'
+
+
+const todos = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
+
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos
+    }
+  } 
+
+  addTodo = (e, item) => {
+    console.log("adding item")
+    // e.preventDefault()
+    // const newTodo = {
+    //   task: item,
+    //   id: Data.now(),
+    //   completed: false 
+    // }
+  }
+
+
+  toggleTodo = (itemId) => {
+    console.log("toggling item", itemId)
+  }
+
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>To-Do</h2>
+        <TodoList todos={todos} />
       </div>
     );
   }
